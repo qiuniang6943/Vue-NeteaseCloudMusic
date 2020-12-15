a3f2673aea464c32074347a98b237f7c4d4f829a
王英龙 <1018786943@qq.com>
2020-12-15 18:43:49 +0800

    登录页样式增加，增加enter键监听触发登陆

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="container">
-    <div class="form">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="手机号码">
-          <el-input v-model="form.phone"></el-input>
-        </el-form-item>
-        <!-- <el-form-item label="验证码">
+    <div class="container" @keyup.enter="login()">
+      <div class="form">
+        <h1 class="loginType">手机号登陆</h1>
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="手机号码">
+            <el-input v-model="form.phone"></el-input>
+          </el-form-item>
+          <!-- <el-form-item label="验证码">
           <el-input v-model="form.code"></el-input>
         </el-form-item> -->
-        <el-form-item label="密码">
-          <el-input v-model="form.pwd" type="password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <!-- <el-button type="primary" @click="getCode">获取验证码</el-button> -->
-          <el-button type="primary" @click="login">登陆</el-button>
-          <!-- <el-button>取消</el-button> -->
-        </el-form-item>
-      </el-form>
-    </div>
+          <el-form-item label="密码">
+            <el-input v-model="form.pwd" type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <!-- <el-button type="primary" @click="getCode">获取验证码</el-button> -->
+            <el-button type="primary" @click="login">登陆</el-button>
+            <!-- <el-button>取消</el-button> -->
+          </el-form-item>
+        </el-form>
+      </div>
   </div>
 </template>
 
 <script>
 import request from "../request/request.js";
-import router from '@/router'
+import router from "@/router";
 export default {
   name: "Login",
   data() {
@@ -46,16 +47,16 @@ export default {
           if (response.data.code === 502) {
             this.$message.error(response.data.message);
           } else {
-            console.log(response.data.cookie)
-            localStorage.setItem('Cookie',response.data.cookie)
-            localStorage.setItem('userID',response.data.account.id)
+            console.log(response.data.cookie);
+            localStorage.setItem("Cookie", response.data.cookie);
+            localStorage.setItem("userID", response.data.account.id);
             // document.cookie = `${response.data.cookie}`
             this.$message({
               message: "登陆成功",
               type: "success",
-            })
-          router.push({ path: '/' })
-          store.commit('loggedIn')
+            });
+            router.push({ path: "/" });
+            store.commit("loggedIn");
           }
         })
         .catch((error) => {
@@ -96,6 +97,12 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  .loginType {
+    font-size: 25px;
+    text-align: center;
+    margin-bottom: 20px;
+    margin-left: 75px;
+  }
   .from {
     width: 300px;
   }
